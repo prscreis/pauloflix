b98eab6e4baac47a2614d9f2590d7d13e65e16e4
Paulo Reis <prsc.reis@gmail.com>
2020-07-30 01:55:44 -0300

    Implementação de roteamento, template e carousel

---
+++ src/pages/cadastro/Categoria/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import PageDefault from "../../../components/PageDefault";
+import { Link } from "react-router-dom";
+
+function CadastroCategoria() {
+	return (
+		<PageDefault>
+			<h1>Cadastro de categoria</h1>
+
+			<form>
+				<label>Nome da categoria:
+					<input type="text" />
+				</label>
+
+				<button>Cadastrar</button>
+			</form>
+
+			<Link to="/">
+				Ir para home
+			</Link>
+		</PageDefault>
+	);
+}
+
+export default CadastroCategoria;
